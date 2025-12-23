5668de96ab5906fb0979231f65b166de5f29517d
Калимуллин Руслан <rkalimullin@synergy.ru>
2025-12-23 11:18:06 +0300

    Шифрование паролей, история команд, копирование текста, настройка терминала, файловый браузер

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.ruslan.terminalssh"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
